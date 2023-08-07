5958efae09e504f2b60561e728b4c6448145d58e
matias3026 <matias3026@gmail.com>
2023-08-07 11:49:26 -0300

    se termina con el header generico

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,37 +1,32 @@
 // src/components/Header/Header.jsx
 import React from 'react';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
+import { FaSuitcase, FaUser } from 'react-icons/fa';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => {
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">Logo</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <img
-          src="https://via.placeholder.com/30"
-          alt="User Avatar"
-          style={{ borderRadius: '50%', marginLeft: '10px' }}
-        />
-      </Navbar.Collapse>
+    <Navbar bg="light" expand="lg" className="d-flex justify-content-between px-3">
+      <Navbar.Brand>
+        <FaSuitcase size={30} style={{ marginRight: '10px' }} />
+        Viajes
+      </Navbar.Brand>
+      <Nav className="d-flex">
+        <Nav.Link href="#link1" className="mr-3">Link 1</Nav.Link>
+        <Nav.Link href="#link2" className="mr-3">Link 2</Nav.Link>
+        <Nav.Link href="#link3" className="mr-3">Link 3</Nav.Link>
+        <Nav.Link href="#link4">Link 4</Nav.Link>
+      </Nav>
+      <Nav>
+        <Nav.Link>
+          <FaUser size={20} style={{ marginRight: '5px' }} />
+          Ingresar
+        </Nav.Link>
+      </Nav>
     </Navbar>
   );
 };
 
 export default Header;
 
-  
 
-  
