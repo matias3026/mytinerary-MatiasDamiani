42317a30536af428953086fdeb8d1eb4edcbc0ac
matias <matias3026@gmail.com>
2023-08-11 22:03:22 -0300

    se comienza con el citis,el resposive me esta desgarrando el alma por no decir el recto

--- src/components/Cities/Cities.jsx
+++ src/components/Cities/Cities.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
-import Header from '../Header/Header'; // Ruta relativa a Header
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 import './Cities.css';
+import citiesData from '../CarouselComponent/cities.json'; // Ruta relativa al archivo cities.json
+
 const Cities = () => {
   return (
-    <div>
-      <Header /> {/* Incluye el Header en Cities */}
-      <h2 className='h2'>Cities </h2>
-      <p className='p'>Aca habria una pagina de Ciudades ... <span>SI TUVIESE UNA !</span> </p>
-    
-      
-    </div>
+    <body className="cities">
+      <header>
+        <Header />
+      </header>
+      <main className="cities2">
+        <h2 className="h2">Cities</h2>
+        <div className="city-cards">
+          {citiesData.map((city, index) => (
+            <div key={index} className="city-card">
+              <h3>{city.name}</h3>
+              <p>Capital: {city.capital}</p>
+              <p>{city.description}</p>
+              <img src={city.image} alt={`${city.name} Image`} /> {/* Renderizar la imagen */}
+              <button>Discover the City</button>
+            </div>
+          ))}
+        </div>
+      </main>
+      <Footer />
+    </body>
   );
 };
 
 export default Cities;
+
